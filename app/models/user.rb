f087054e8db16be2d9a41e8728703b11dead92ac
345dvl <k.f.kntn@gmail.com>
2020-11-04 12:44:39 +0900

    Userモデルにバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :description, length: { maximum: 150 }
 end
