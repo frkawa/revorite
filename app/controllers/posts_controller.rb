7c5378ccd3228277faf5647183f1ae5fc3a9499d
345dvl <k.f.kntn@gmail.com>
2020-11-26 14:12:51 +0900

    投稿削除機能、フラッシュメッセージ追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,10 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all.includes(:user)
+    if user_signed_in?
+      @post_count = Post.where(user_id: current_user.id).count
+    else
+    end
   end
 
   def new
@@ -9,13 +13,30 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(post_params)
-    redirect_to :root
+    @post = Post.new(post_params)
+    if @post.save 
+      redirect_to :root, notice: "投稿に成功しました"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    post = Post.find_by(id: params[:id])
+    if post.present?
+      if post.destroy
+        redirect_to root_path, notice: "投稿を削除しました"
+      else
+        redirect_to root_path, alert: "投稿の削除に失敗しました"
+      end
+    else
+      redirect_to root_path, alert: "投稿が見つかりません"
+    end
   end
 
   private
   def post_params
-    params.require(:post).permit(:name, :text).merge(user_id: current_user.id)
+    params.require(:post).permit(:name, :text, images: []).merge(user_id: current_user.id)
   end
   
 end
