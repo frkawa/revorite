dc57d461841834aa939a8a1105e3d49c5e33dc86
345dvl <k.f.kntn@gmail.com>
2021-01-24 18:10:06 +0900

    確認用パスワードのバリデーションチェック（フロント）、ボタンの活性化/非活性化実装

--- app/assets/javascripts/devise.js
+++ app/assets/javascripts/devise.js
@@ -1,8 +1,13 @@
 $(function (){
+  var name_available_flg = "ng";
+  var email_available_flg = "ng";
+  var password_available_flg = "ng";
+  var password_confirmation_available_flg = "ng";
+
   // 名前の入力文字数が30文字以内かどうかをチェック、31文字以上の場合は警告を出す
   $("#signup-input-name").change(function (){
     var namelength = $(this).val().length;
-    var name_available_flg = "ng";
+    // var name_available_flg = "ng";
     
     if(namelength > 30){
       $(this).addClass("input-caution");
@@ -17,12 +22,15 @@ $(function (){
       $("#signup-caution-name").text("");
       name_available_flg = "ok";
     }
+
+    console.log(name_available_flg + email_available_flg + password_available_flg + password_confirmation_available_flg);
+    buttonToggle(name_available_flg, email_available_flg, password_available_flg, password_confirmation_available_flg);
   })
 
   // メールアドレス
   $("#signup-input-email").change(function (){
     var VALID_EMAIL_REGEX_EMAIL = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-    var email_available_flg = "ng";
+    // var email_available_flg = "ng";
     if($(this).val() == ''){
       $(this).addClass("input-caution");
       $("#signup-caution-email").text("メールアドレスを入力してください");
@@ -36,29 +44,70 @@ $(function (){
       $("#signup-caution-email").text("");
       email_available_flg = "ok";
     }
+    console.log(name_available_flg + email_available_flg + password_available_flg + password_confirmation_available_flg);
+    buttonToggle(name_available_flg, email_available_flg, password_available_flg, password_confirmation_available_flg);
   })
 
   // パスワード
   $("#signup-input-password").change(function (){
     var passwordlength = $(this).val().length;
     var VALID_EMAIL_REGEX_PASSWORD = /^[a-zA-Z\d]+$/
-    var password_available_flg = "ng";
+    // var password_available_flg = "ng";
     if(passwordlength == 0){
       $(this).addClass("input-caution");
       $("#signup-caution-password").text("パスワードを入力してください");
-      email_available_flg = "ng";
+      password_available_flg = "ng";
     } else if(passwordlength < 6 || passwordlength > 30){
       $(this).addClass("input-caution");
       $("#signup-caution-password").text("パスワードは6文字以上30文字以内で入力してください");
-      email_available_flg = "ng";
+      password_available_flg = "ng";
     } else if(!$(this).val().match(VALID_EMAIL_REGEX_PASSWORD)) {
       $(this).addClass("input-caution");
       $("#signup-caution-password").text("半角英数字以外の文字は利用できません");
-      email_available_flg = "ng";
+      password_available_flg = "ng";
     } else {
       $(this).removeClass("input-caution");
       $("#signup-caution-password").text("");
-      email_available_flg = "ok";
+      password_available_flg = "ok";
     }
+
+    if($("#signup-input-password_confirmation").val().length != 0){
+    password_confirmation_check();
+    }
+    console.log(name_available_flg + email_available_flg + password_available_flg + password_confirmation_available_flg);
+    buttonToggle(name_available_flg, email_available_flg, password_available_flg, password_confirmation_available_flg);
   })
+
+  // （確認用）パスワード
+  $("#signup-input-password_confirmation").change(function (){
+    password_confirmation_check();
+    console.log(name_available_flg + email_available_flg + password_available_flg + password_confirmation_available_flg);
+    buttonToggle(name_available_flg, email_available_flg, password_available_flg, password_confirmation_available_flg);
+  })
+
+  function password_confirmation_check() {
+    // var password_confirmation_available_flg = "ng";
+    if($("#signup-input-password_confirmation").val().length == 0){
+      $("#signup-input-password_confirmation").addClass("input-caution");
+      $("#signup-caution-password_confirmation").text("確認用にパスワードをもう一度入力してください");
+      password_confirmation_available_flg = "ng";
+    } else if($("#signup-input-password_confirmation").val() != $("#signup-input-password").val()){
+      $("#signup-input-password_confirmation").addClass("input-caution");
+      $("#signup-caution-password_confirmation").text("パスワードと一致していません");
+      password_confirmation_available_flg = "ng";
+    } else {
+      $("#signup-input-password_confirmation").removeClass("input-caution");
+      $("#signup-caution-password_confirmation").text("");
+      password_confirmation_available_flg = "ok";
+    }
+  }
+
+  // 全ての入力値が問題無ければ、登録ボタンを活性化。そうでなければ非活性化する
+  function buttonToggle(name_available_flg, email_available_flg, password_available_flg, password_confirmation_available_flg) {
+    if(name_available_flg == "ok" && email_available_flg == "ok" && password_available_flg == "ok" && password_confirmation_available_flg == "ok") {
+      $("#signup-button-submit").removeAttr("disabled");
+    } else {
+      $("#signup-button-submit").attr("disabled", true);
+    }
+  }
 })
